6bd9fe84b330660d3bba56eb36f28aecc20be1e8
Janice927-mei <janicelim885@gmail.com>
2026-08-21 14:34:08 +0800

    Create payment class with payment methods

---
+++ payment.java
@@ -0,0 +1,11 @@
+public class payment {
+    // e-Wallet, Credit Card, or Online Banking
+    public boolean makePayment(double amount, String paymentMethod) {
+        return false; 
+    }
+    
+    public double getPaymentAmount() { return 0.0; }
+    public String getPaymentMethod() { return null; }
+    public String getPaymentStatus() { return null; }
+    public void updateOrderStatus(printOrder order, boolean isSuccess) {}
+}
