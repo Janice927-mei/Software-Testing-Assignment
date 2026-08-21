a7043ce3cfab29d8f2d533c5bbe1f3e2e85c24b8
Janice927-mei <janicelim885@gmail.com>
2026-08-21 14:33:40 +0800

    Add printerAvailability class with availability method

---
+++ printerAvailability.java
@@ -0,0 +1,7 @@
+// 6) printerAvailability.java
+public class printerAvailability {
+    // To be mocked using Mockito
+    public boolean isPrinterAvailable(String paperSize, String printType) {
+        return false; // Dummy return, logic is handled by Mockito during testing
+    }
+}
