f4917571d79512c05d819bc76a7b69580ad253d5
Janice927-mei <janicelim885@gmail.com>
2026-08-25 01:42:54 +0800

    Update payment.java

--- payment.java
+++ payment.java
@@ -1,11 +1,6 @@
 public class payment {
-    // e-Wallet, Credit Card, or Online Banking
+
     public boolean makePayment(double amount, String paymentMethod) {
-        return false; 
+        return false;
     }
-    
-    public double getPaymentAmount() { return 0.0; }
-    public String getPaymentMethod() { return null; }
-    public String getPaymentStatus() { return null; }
-    public void updateOrderStatus(printOrder order, boolean isSuccess) {}
 }
