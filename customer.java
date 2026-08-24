ce2fb82be342caab827bfbbe3291b6c974331617
Janice927-mei <janicelim885@gmail.com>
2026-08-25 01:41:38 +0800

    Refactor customer class and update fields
    Refactor customer class to improve variable names and add previousOrders field.

--- customer.java
+++ customer.java
@@ -1,25 +1,54 @@
-// 1) customer.java
+package ApplicationCode;
+
 public class customer {
     private String customerID;
     private String name;
-    private String email;
-    private String phone;
+    private String emailAddress;
+    private String phoneNumber;
     private String customerType;
+    private int previousOrders;
 
-    public customer(String customerID, String name, String email, String phone, String customerType) {
+    public customer(String customerID, String name, String emailAddress,
+                    String phoneNumber, String customerType, int previousOrders) {
         this.customerID = customerID;
         this.name = name;
-        this.email = email;
-        this.phone = phone;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
         this.customerType = customerType;
+        this.previousOrders = previousOrders;
     }
 
-    // Getters
-    public String getCustomerID() { return customerID; }
-    public String getName() { return name; }
-    public String getEmail() { return email; }
-    public String getPhone() { return phone; }
-    public String getCustomerType() { return customerType; }
-    
-    // Setters can be added as needed...
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public int getPreviousOrders() {
+        return previousOrders;
+    }
+
+    public void setPreviousOrders(int previousOrders) {
+        this.previousOrders = previousOrders;
+    }
+
+    @Override
+    public String toString() {
+        return customerID + "," + name + "," + emailAddress + ","
+                + phoneNumber + "," + customerType + "," + previousOrders;
+    }
 }
