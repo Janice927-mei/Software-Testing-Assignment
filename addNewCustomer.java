c57b1feab8e74bfdd7bb50032a0770e052b55712
le0715 <ahmoi899@gmail.com>
2026-08-28 16:44:10 +0800

    Update addNewCustomer.java

--- addNewCustomer.java
+++ addNewCustomer.java
@@ -4,8 +4,17 @@ import java.io.PrintWriter;
 
 public class addNewCustomer {
 
-    private static final String FILE_NAME = "customer.txt";
+    private static final String DEFAULT_FILE_NAME = "customer.txt";
 
+    private final String fileName;
+
+    public addNewCustomer() {
+        this(DEFAULT_FILE_NAME);
+    }
+
+    public addNewCustomer(String fileName) {
+        this.fileName = fileName;
+    }
     public void addCustomer(customer newCustomer) throws IOException {
         PrintWriter writer = new PrintWriter(new FileWriter(FILE_NAME, true));
 
